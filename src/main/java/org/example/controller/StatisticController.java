762756bf87a77b087243f09f091150e267497a2b
Nguyen Thanh Tung <thanhtung232004@gmail.com>
2025-12-30 20:58:54 +0700

    daskboard statistic

---
+++ src/main/java/org/example/controller/StatisticController.java
@@ -0,0 +1,28 @@
+package org.example.controller;
+
+import org.example.service.StatisticService;
+import org.example.service.impl.StatisticServiceImpl;
+
+public class StatisticController {
+    private final StatisticService statisticService = new StatisticServiceImpl();
+
+    public int getCustomerCount() {
+        return statisticService.getCustomerCount();
+    }
+
+    public int getEmployeeCount() {
+        return statisticService.getEmployeeCount();
+    }
+
+    public double getMonthlyRevenue() {
+        return statisticService.getMonthlyRevenue();
+    }
+
+    public int getReservationCount() {
+        return statisticService.getReservationCount();
+    }
+
+    public String getBestSellingProduct() {
+        return statisticService.getBestSellingProduct();
+    }
+}
